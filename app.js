269248e61d43d0dbb80489c75885f47aed10bfd0
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-03-28 16:23:55 +0100

    login added

--- app.js
+++ app.js
@@ -2,20 +2,87 @@ const express = require('express')
 const path = require('path');
 const app = express()
 const axios = require('axios')
+const cookieParser = require('cookie-parser');
+const sessions = require('express-session');
+const querystring = require('querystring');
 
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 
+const hour = 1000 * 60 * 60 * 1;
+
+app.use(sessions({
+  secret: "letmein12356",
+  saveUninitialized: true,
+  cookie: { maxAge: hour },
+  resave: false
+}));
+
 app.get('/', (req, res) => {
 
-  let page = req.query.page;
+  let sessionObj = req.session
+
+  console.log(sessionObj)
+
+  // let page = req.query.page;
 
   let ep = `http://localhost:4000/collections`
 
   axios.get(ep).then((response) => {
     let data = response.data;
-    res.render('collections', { data })
+    res.render('collections', { data, user: sessionObj.sess_user })
+  })
+})
+
+app.get('/login', (req, res) => {
+  let sessionObj = req.session;
+  if (sessionObj.sess_valid) {
+    res.redirect('/')
+  } else {
+    res.render('login')
+  }
+
+})
+
+app.post('/login', (req, res) => {
+
+  let sessionObj = req.session;
+
+  let user_name = req.body.username;
+  let password = req.body.password;
+
+  let ep = `http://localhost:4000/login`
+
+  axios.post(ep, querystring.stringify({ user_name, password })).then(response => {
+
+    console.log(response)
+
+    let data = response.data;
+
+    if (data.success) {
+      sessionObj.sess_valid = true;
+      sessionObj.sess_user = response.data.user;
+      res.redirect('/')
+    } else {
+      res.render('login', { data })
+    }
+
+  }).catch((error) => {
+    console.log(error);
+  })
+
+
+})
+
+
+app.get('/register', (req, res) => {
+
+  let ep = `http://localhost:4000/albumart`
+
+  axios.get(ep).then((response) => {
+    let data = response.data;
+    res.render('register', { data })
   })
 })
 
@@ -46,23 +113,32 @@ app.post('/addcollection', (req, res) => {
 
   let collectionName = req.body.collectionname;
   let albumIDStringArray = req.body.album_id;
+  let user_id = req.session.sess_user.user_id;
 
-  let albumIDs = albumIDStringArray.map(Number)
+  if (albumIDStringArray != null) {
 
-  console.log(collectionName)
-  console.log(albumIDs)
+    let albumIDs;
+
+    if (typeof albumIDStringArray === 'string') {
+      albumIDs = [Number(albumIDStringArray)]
+    } else {
+      albumIDs = albumIDStringArray.map(Number)
+    }
+    console.log(user_id)
+    console.log(collectionName)
+    console.log(albumIDs)
 
 
-  let ep = `http://localhost:4000/addcollection`
+    let ep = `http://localhost:4000/addcollection`
 
-  // set up api end point
+    // set up api end point
 
-  axios.post(ep).then((response) => {
+    // axios.post(ep).then((response) => {
 
-    // set up success/failure view with link to created collection
+    //   // set up success/failure view with link to created collection
 
-  })
-
+    // })
+  }
 })
 
 app.get('/albums', (req, res) => {
