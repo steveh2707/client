cdba38da45c96244820cf75ec4708f9a0428cee6
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-04-17 10:53:05 +0100

    pagination for all collections

--- routes/collectionsAllRoute.js
+++ routes/collectionsAllRoute.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
+const querystring = require('querystring');
 
 router.get('/', (req, res) => {
-  // let page = req.query.page;
-  let ep = `http://localhost:4000/collections`
+  let page = req.query.page || 1;
+
+  console.log(page)
+
+  let ep = `http://localhost:4000/collections?page=${page}`
+
+  // axios.post(ep, querystring.stringify({ c_id, comment_message, user_id })).then(response => {
 
   axios.get(ep).then((response) => {
     let responseData = response.data;
 
     if (responseData.success) {
-      let data = responseData.data
-      res.render('allcollections', { data })
+      // let data = responseData.data
+      res.render('allcollections', { responseData })
     }
   })
 })
