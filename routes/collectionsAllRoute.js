c654ab45e23fa654d068c5c3115bfe610c8da118
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-04-19 11:29:24 +0100

    sort collections by likes and comments

--- routes/collectionsAllRoute.js
+++ routes/collectionsAllRoute.js
@@ -6,16 +6,36 @@ const querystring = require('querystring');
 router.get('/', (req, res) => {
   let page = req.query.page || 1;
 
-  let ep = `http://localhost:4000/collections?page=${page}`
+  let sortQuery = req.query.sort
 
-  // axios.post(ep, querystring.stringify({ c_id, comment_message, user_id })).then(response => {
+  let currentSort = ""
+  let otherSortOptions = []
+
+  let editedSort = { name: "Last Edited", link: "edit" }
+  let likesSort = { name: "Likes", link: "likes" }
+  let commentSort = { name: "Comments", link: "comments" }
+
+  if (sortQuery === "likes") {
+    currentSort = likesSort
+    otherSortOptions.push(editedSort, commentSort)
+  } else if (sortQuery === "comments") {
+    currentSort = commentSort
+    otherSortOptions.push(editedSort, likesSort)
+  } else {
+    sortQuery = "edit"
+    currentSort = editedSort
+    otherSortOptions.push(likesSort, commentSort)
+  }
+
+  let ep = `http://localhost:4000/collections?page=${page}&sort=${sortQuery}`
+
 
   axios.get(ep).then((response) => {
     let responseData = response.data;
 
     if (responseData.success) {
       // let data = responseData.data
-      res.render('allcollections', { responseData })
+      res.render('allcollections', { responseData, currentSort, otherSortOptions })
     }
   })
 })
